498ae4df2eab7824576da032ea453aedb365f496
ayush.sharma <ayush.sharm@olx.com>
2023-11-30 13:10:56 +0530

    sql downsampling for h2

--- src/main/java/com/example/astuto/demo/repository/MetricRepository.java
+++ src/main/java/com/example/astuto/demo/repository/MetricRepository.java
@@ -1,5 +1,6 @@
 package com.example.astuto.demo.repository;
 
+import com.example.astuto.demo.dto.MetricDTO;
 import com.example.astuto.demo.model.Metric;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,16 @@ public interface MetricRepository extends JpaRepository<Metric, Long> {
     List<Metric> findByDeviceIdAndTimestampBetween(String deviceId, Long startTimeStamp, Long endTimeStamp);
 
     Long countByDeviceIdAndTimestampBetween(String deviceId, Long startTimeStamp, Long endTimeStamp);
+
+    @Query(value = "SELECT new com.example.astuto.demo.dto.MetricDTO(" +
+            "m.deviceId, " +
+            "AVG(m.energyConsumed), " +
+            "TRUNC(m.timestamp / 3600) * 3600) " +
+            "FROM Metric m " +
+            "WHERE m.timestamp BETWEEN :startTimestamp AND :endTimestamp " +
+            "AND m.deviceId = :deviceId " +
+            "GROUP BY m.deviceId, TRUNC(m.timestamp / 3600)",
+            nativeQuery = true)
+    List<MetricDTO> findDownsampledByDeviceIdAndTimestampBetween(String deviceId, Long startTimestamp, Long endTimestamp);
+
 }
